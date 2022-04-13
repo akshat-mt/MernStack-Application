2cbc42d9aaab5846d321f00051dcdb85d2fb1e89
Akshat Mani Tripathi <akshatmani1010@gmail.com>
2022-04-14 00:15:00 +0530

    inital phase complete

--- helper/jwt.js
+++ helper/jwt.js
@@ -13,7 +13,7 @@ function authJwt () {
             {url:/\/api\/v1\/categories(.*)/ ,methods:['GET','OPTIONS']},
             `${api}/users/login`,,
             `${api}/users/register`
-        ]
+        ],
     })
 }
 
